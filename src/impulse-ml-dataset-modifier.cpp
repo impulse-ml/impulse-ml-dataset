d232cc547896058c4d32e5d44df03146c9810760
hud <baniczek@gmail.com>
2020-07-27 08:13:22 +0200

    modifier

--- src/impulse-ml-dataset-modifier.cpp
+++ src/impulse-ml-dataset-modifier.cpp
@@ -25,6 +25,32 @@ int main(int argc, char *argv[]) {
             DatasetBuilder::CSVBuilder builder(filenameIn);
             Dataset dataset = builder.build();
 
+            if (cmdOptionExists(argv, argv + argc, "--modifier") && cmdOptionExists(argv, argv + argc, "--columns")) {
+                char * modifier = getCmdOption(argv, argv + argc, "--modifier");
+                char * columns = getCmdOption(argv, argv + argc, "--columns");
+
+                if (modifier && columns) {
+                    std::stringstream test(columns);
+                    std::string segment;
+
+                    while (std::getline(test, segment, ',')) {
+                        if (T_String(modifier) == "category") {
+                            DatasetModifier::Modifier::Category modifier2(dataset);
+                            modifier2.applyToColumn(std::atoi(segment.c_str()));
+                        } else if (T_String(modifier) == "minmax") {
+                            DatasetModifier::Modifier::MinMaxScaling modifier2(dataset);
+                            modifier2.applyToColumn(std::atoi(segment.c_str()));
+                        } else if (T_String(modifier) == "zscores") {
+                            DatasetModifier::Modifier::ZScoresScaling modifier2(dataset);
+                            modifier2.applyToColumn(std::atoi(segment.c_str()));
+                        } else if (T_String(modifier) == "missing") {
+                            DatasetModifier::Modifier::MissingData modifier2(dataset);
+                            modifier2.applyToColumn(std::atoi(segment.c_str()));
+                        }
+                    }
+                }
+            }
+
             if (cmdOptionExists(argv, argv + argc, "-o")) {
                 char *filenameOut = getCmdOption(argv, argv + argc, "-o");
 
